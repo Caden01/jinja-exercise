384cc18991bbb0993abb70937603502f1fe6ea08
Caden Carlson <caden@Cadens-MacBook-Pro.local>
2023-12-14 07:51:46 -0800

    Add story page that shows a madlibs story with words given by user

--- app.py
+++ app.py
@@ -13,3 +13,9 @@ debug = DebugToolbarExtension(app)
 def home() :
     prompts = story.prompts
     return render_template("home.html", prompts=prompts)
+
+@app.route("/story")
+def show_story() :
+    text = story.generate(request.args)
+
+    return render_template("story.html", text=text)
